7752966e6d32d8874db0c6f1254215b99741b85d
Mikołaj Styś <ms@human-device.com>
2016-07-14 18:03:53 +0200

    Initial Commit

---
+++ HDLogger/HDLogger.h
@@ -0,0 +1,19 @@
+//
+//  HDLogger.h
+//  HDLogger
+//
+//  Created by Mikołaj Styś on 14.07.2016.
+//  Copyright © 2016 Human Device Sp. z.o.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDLogger.
+FOUNDATION_EXPORT double HDLoggerVersionNumber;
+
+//! Project version string for HDLogger.
+FOUNDATION_EXPORT const unsigned char HDLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDLogger/PublicHeader.h>
+
+
